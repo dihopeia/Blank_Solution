d9512ac0e77407619a09226227ab9ec608ccacbc
Horváth Gábor <dihopeia@gmail.com>
2019-05-16 15:44:49 +0200

    -DeliveryAddress+CustomerDetails table

---
+++ Blank_Solution/Web/Models/DeliveryAddress.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class DeliveryAddress
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string ZipCode { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+    }
+}
